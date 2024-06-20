c1fa735eb0513f007bfceca666f4402d68fba1c6
Soumyadip Ojha <soumyadipojha635@gmail.com>
2024-06-20 09:32:49 +0530

    frontend

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
         <h1 className="text-4xl font-bold text-center pb-3 mb-8 mt-8 text-blue-400">
           Facilities We Provide with Care
         </h1>
-        <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ml-10">
+        <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
           <div className="card card-compact w-96 bg-base-100 shadow-xl">
             <figure>
               <img
@@ -207,7 +207,7 @@ const Hero = () => {
       <h1 className="text-4xl font-bold text-center mb-8 mt-8 text-blue-400">
         What people say about us{" "}
       </h1>
-      <div className="join join-vertical w-9/12 ml-48">
+      <div className="join join-vertical w-9/12 ml-20">
         <div className="collapse collapse-arrow join-item border border-base-300">
           <input type="radio" name="my-accordion-4" defaultChecked />
           <div className="collapse-title text-xl font-medium">
