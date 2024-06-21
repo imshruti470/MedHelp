980959b5e6e4ae30f7035f75ba05ec2502ffa096
Soumyadip Ojha <soumyadipojha635@gmail.com>
2024-06-21 21:35:08 +0530

    added some more details

--- src/components/Meeting.jsx
+++ src/components/Meeting.jsx
@@ -1,39 +1,43 @@
-import React from 'react';
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
-const Meeting = () => {
+export default function App() {
   const roomID = "Room";
-
-  const myMeeting = async (element) => {
-    // Generate Kit Token
+  let myMeeting = async (element) => {
+    // generate Kit Token
     const appID = 362239873;
     const serverSecret = "3bca63f97faf78d0aef796b7dfb250f4";
-    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, Date.now().toString(), "Marko");
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      Date.now().toString(),
+      "Marko"
+    );
 
     // Create instance object from Kit Token.
     const zp = ZegoUIKitPrebuilt.create(kitToken);
-
-    // Start the call
+    // start the call
     zp.joinRoom({
       container: element,
       sharedLinks: [
         {
-          name: 'Personal link',
-          url: window.location.protocol + '//' + window.location.host + window.location.pathname + '?roomID=' + roomID,
+          name: "Personal link",
+          url:
+            window.location.protocol +
+            "//" +
+            window.location.host +
+            window.location.pathname +
+            "?roomID=" +
+            roomID,
         },
       ],
       scenario: {
-        mode: ZegoUIKitPrebuilt.OneONoneCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+        mode: [ZegoUIKitPrebuilt.OneONoneCall], // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
       },
     });
   };
 
   return (
-    <div
-      ref={myMeeting}
-      className="w-full h-screen md:w-[100vw] md:h-[100vh]" // Adjusting width and height for responsiveness
-    ></div>
+    <div ref={myMeeting} style={{ width: "100vw", height: "100vh" }}></div>
   );
-};
-
-export default Meeting;
+}
